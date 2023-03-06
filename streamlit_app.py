3c2c2ead05c8a8bbc63ba34f1391c70c666191e9
Jsnowfla <126892905+Jsnowfla@users.noreply.github.com>
2023-03-06 12:03:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -19,3 +18,5 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.dataframe(fruits_to_show)
 
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
