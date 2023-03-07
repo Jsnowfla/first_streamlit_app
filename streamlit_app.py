10f357352dfbb6afdfc8fa38d85397573ff3fde1
Jsnowfla <126892905+Jsnowfla@users.noreply.github.com>
2023-03-07 09:23:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,6 @@ try:
 except URLError as e:
   streamlit.error()
   
-#dont't run anything past here while ...
-#streamlit.stop()
-
 #Snowflake function
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -52,6 +49,9 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
+
+#dont't run anything past here while ...
+streamlit.stop()
 
     
 fruit_added = streamlit.text_input('What fruit would you like to add?','Jackfruit')
